ab191edf5c3bacd85edb8a882d2d091afef52266
Gábor <szilgyigbor@gmail.com>
2024-04-17 11:56:54 +0200

    Add DbContextfile for Entity framework

---
+++ Ol_der/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ol_der.Models;
+
+namespace Ol_der.Data
+{
+    class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+
+    }
+}
