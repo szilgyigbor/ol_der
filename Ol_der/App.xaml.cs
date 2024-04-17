62f90d86941c797e154b4528d90bce92dbe6b65f
Gábor <szilgyigbor@gmail.com>
2024-04-17 12:09:41 +0200

    Configure database context.

--- Ol_der/App.xaml.cs
+++ Ol_der/App.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Ol_der.Data;
+using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Windows;
 
 namespace Ol_der
@@ -9,6 +13,25 @@ namespace Ol_der
     /// </summary>
     public partial class App : Application
     {
+        private IConfigurationRoot Configuration;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            Configuration = builder.Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+
+            using (var context = new ApplicationDbContext(optionsBuilder.Options))
+            {
+                context.Database.EnsureCreated();
+            }
+
+            base.OnStartup(e);
+        }
     }
 
 }
