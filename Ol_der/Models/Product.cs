7fd591acf3579be3712dc41a0e74d73a56a637b9
Gábor <szilgyigbor@gmail.com>
2024-04-18 18:53:17 +0200

    Add ProductId to Product class

--- Ol_der/Models/Product.cs
+++ Ol_der/Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,8 @@ namespace Ol_der.Models
 {
     class Product
     {
+        public int ProductId { get; set; }
+        
         public string ItemNumber { get; set; }
         public string Name { get; set; }
         public int SupplierId { get; set; }
