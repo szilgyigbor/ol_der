88df5e7121e315803e6a0f8e24daa67bad16786a
Gábor <szilgyigbor@gmail.com>
2024-04-19 18:05:54 +0200

    Add new migration

---
+++ Ol_der/Migrations/20240419160456_UpdateSupplier.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ol_der.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSupplier : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Suppliers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Suppliers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Suppliers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Suppliers");
+        }
+    }
+}
