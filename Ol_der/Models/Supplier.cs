6e974d84005406d9a6a020e01ab5a6edcc55a564
Gábor <szilgyigbor@gmail.com>
2024-04-19 18:04:08 +0200

    Upgrade supplier model

--- Ol_der/Models/Supplier.cs
+++ Ol_der/Models/Supplier.cs
@@ -10,6 +10,9 @@ namespace Ol_der.Models
     {
         public int SupplierId { get; set; }
         public string Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
         public List<Product> Products { get; set; }
     }
 }
