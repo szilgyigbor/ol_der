7c2631a6d43e45f1dd06a89c3438b3c5e5d8d408
Gábor <szilgyigbor@gmail.com>
2024-04-19 21:11:29 +0200

    Add ShowAllSupplierControl with list view

---
+++ Ol_der/Controls/Suppliers/ShowAllSupplierControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Ol_der.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ol_der.Controls.Suppliers
+{
+    /// <summary>
+    /// Interaction logic for ShowAllSupplierControl.xaml
+    /// </summary>
+    public partial class ShowAllSupplierControl : UserControl
+    {
+        public ShowAllSupplierControl()
+        {
+            InitializeComponent();
+        }
+
+        public void DisplaySuppliers(List<Supplier> suppliers)
+        {
+            SuppliersListView.ItemsSource = suppliers;
+        }
+    }
+}
