9644c8969b9f250236072a3353d79ce973185c3a
Gábor <szilgyigbor@gmail.com>
2024-04-23 21:29:35 +0200

    Add SearchProductControl with required methods

---
+++ Ol_der/Controls/Products/SearchProductControl.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Ol_der.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ol_der.Controls.Products
+{
+    /// <summary>
+    /// Interaction logic for SearchProductControl.xaml
+    /// </summary>
+    public partial class SearchProductControl : UserControl
+    {
+        public event Action<string> OnProductId;
+
+        public SearchProductControl()
+        {
+            InitializeComponent();
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            OnProductId?.Invoke(itemNumberTextBox.Text);
+        }
+    }
+}
