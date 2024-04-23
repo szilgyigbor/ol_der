9f8e7de27de5358083cb0c5c10e4f93c62ff0b64
Gábor <szilgyigbor@gmail.com>
2024-04-23 20:34:40 +0200

    Add PaymentType model

---
+++ Ol_der/Models/PaymentType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ol_der.Models
+{
+    public enum PaymentType
+    {
+        Cash,
+        Card,
+        RN
+    }
+}
