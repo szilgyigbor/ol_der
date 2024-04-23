7b6282217ceed21d35ca7aab64205f2fe998db63
Gábor <szilgyigbor@gmail.com>
2024-04-23 20:34:27 +0200

    Rework sale model

--- Ol_der/Models/Sale.cs
+++ Ol_der/Models/Sale.cs
@@ -9,9 +9,13 @@ namespace Ol_der.Models
     public class Sale
     {
         public int SaleId { get; set; }
-        public string ItemNumber { get; set; }
-        public int Quantity { get; set; }
+        public ICollection<SaleItem> SaleItems { get; set; } = new List<SaleItem>();
         public DateTime Date { get; set; }
-        public bool IsOrdered { get; set; }
+        public PaymentType PaymentType { get; set; }
+        public decimal TotalAmount { get; set; }
+        public decimal? CardTransactionAmount { get; set; }
+        public bool IsCardTransactionProcessed { get; set; }
+        public string CustomerName { get; set; }
+        public string Notes { get; set; }
     }
 }
