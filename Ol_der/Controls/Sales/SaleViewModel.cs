5572f8c2544e2c1edf0b367d5e92497a49f9266f
Gábor <szilgyigbor@gmail.com>
2024-04-26 12:06:45 +0200

    Add SaleViewModel

---
+++ Ol_der/Controls/Sales/SaleViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Ol_der.Data;
+using Ol_der.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ol_der.Controls.Sales
+{
+    internal class SaleViewModel : IDisposable
+    {
+        private ApplicationDbContext _context;
+
+        public SaleViewModel()
+        {
+            _context = ApplicationDbContextFactory.Create();
+        }
+
+        public void AddSale(Sale newSale)
+        {
+            _context.Sales.Add(newSale);
+            _context.SaveChanges();
+        }
+
+        public Product SearchProductByItemNumber(string itemNumber)
+        {
+            var upperitemNumber = itemNumber.ToUpper();
+            return _context.Products
+                           .FirstOrDefault(p => p.ItemNumber.ToUpper().Contains(upperitemNumber));
+        }
+
+        public void Dispose()
+        {
+            _context?.Dispose();
+        }
+    }
+}
