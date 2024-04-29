f3a8d284ce3beb5aba612b16360179e870cdcd38
Gábor <szilgyigbor@gmail.com>
2024-04-29 19:52:26 +0200

    Add new migration

---
+++ Ol_der/Migrations/20240429160806_SetAutoId.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ol_der.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetAutoId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
