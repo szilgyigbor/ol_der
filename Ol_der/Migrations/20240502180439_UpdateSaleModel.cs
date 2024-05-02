cf354ffa4e73f58e58e62d7f19e21c05ddeef51c
Gábor <szilgyigbor@gmail.com>
2024-05-02 20:26:21 +0200

    Add new migration

---
+++ Ol_der/Migrations/20240502180439_UpdateSaleModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ol_der.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSaleModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CardTransactionAmount",
+                table: "Sales");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "CardTransactionAmount",
+                table: "Sales",
+                type: "decimal(18,2)",
+                precision: 18,
+                scale: 2,
+                nullable: true);
+        }
+    }
+}
