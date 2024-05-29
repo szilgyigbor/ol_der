714216941db57746ccb78aca9199a8b31393e730
Gábor <szilgyigbor@gmail.com>
2024-05-29 11:05:31 +0200

    Add Order model

---
+++ Ol_der/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ol_der.Models
+{
+    public class Order
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int SupplierId { get; set; }
+        public Supplier Supplier { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+        public bool IsConfirmed { get; set; }
+    }
+}
