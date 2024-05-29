2d13653b1a34099df6a0702c3bf72daea25f53d0
Gábor <szilgyigbor@gmail.com>
2024-05-29 11:05:38 +0200

    Add OrderItem model

---
+++ Ol_der/Models/OrderItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Ol_der.Models
+{
+    public class OrderItem
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OrderItemId { get; set; }
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int QuantityOrdered { get; set; }
+        public int QuantityReceived { get; set; }
+        public decimal Price { get; set; }
+        public bool IsArrived { get; set; }
+    }
+}
