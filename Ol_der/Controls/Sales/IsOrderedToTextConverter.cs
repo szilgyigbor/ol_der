3a6a803daba69578e0ca4c376c68aedd77c8762c
Gábor <szilgyigbor@gmail.com>
2024-05-29 10:35:52 +0200

    Add IsOrderedToTextConverter

---
+++ Ol_der/Controls/Sales/IsOrderedToTextConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Ol_der.Controls.Sales
+{
+    public class IsOrderedToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool && (bool)value)
+            {
+                return "(Be lett rendelve)";
+            }
+            else
+            {
+                return "(Még nincs rendelve)";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
