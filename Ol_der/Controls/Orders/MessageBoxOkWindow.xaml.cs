be0633523f4802a0ea7bc07753286e22c34f74c0
Gábor <szilgyigbor@gmail.com>
2024-06-02 17:55:50 +0200

    Add MessageBoxOkWindow for user messages

---
+++ Ol_der/Controls/Orders/MessageBoxOkWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Ol_der.Controls.Orders
+{
+    /// <summary>
+    /// Interaction logic for MessageBoxOkWindow.xaml
+    /// </summary>
+    public partial class MessageBoxOkWindow : Window
+    {
+        public MessageBoxOkWindow(string message)
+        {
+            InitializeComponent();
+            SetMessage(message);
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        public void SetMessage(string message)
+        {
+            messageToShow.Text = message;
+        }
+    }
+}
