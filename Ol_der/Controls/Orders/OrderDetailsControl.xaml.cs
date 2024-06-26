dfdf6b3e085d3fda6f52e2546742a7fe3104a57d
Gábor <szilgyigbor@gmail.com>
2024-06-26 19:07:08 +0200

    Add OrderDetailsControl UI component
    This commit introduces a new UserControl, OrderDetailsControl, within the Ol_der.Controls.Orders namespace, designed to display detailed information about orders. The control features a Grid layout with a TextBox for the title and a ListView for listing order items, including quantity ordered/received, item number, product name, and comments. Additionally, a StackPanel is used for displaying order-related comments. The corresponding code-behind file establishes the control's functionality, including initializing the OrderDetailsViewModel with an orderId and setting the DataContext. The OrderDetailsViewModel class, also added in this commit, manages data fetching for the order details using an OrderRepository and updates the UI accordingly.

---
+++ Ol_der/Controls/Orders/OrderDetailsControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ol_der.Controls.Orders
+{
+    /// <summary>
+    /// Interaction logic for OrderDetailsControl.xaml
+    /// </summary>
+    public partial class OrderDetailsControl : UserControl
+    {
+
+        private OrderDetailsViewModel _viewModel;
+
+        public OrderDetailsControl(int orderId)
+        {
+            InitializeComponent();
+            _viewModel = new(orderId);
+            this.DataContext = _viewModel;
+        }
+    }
+}
---
+++ Ol_der/Controls/Orders/OrderDetailsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Ol_der.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ol_der.Controls.Orders
+{
+    internal class OrderDetailsViewModel
+    {
+        private OrderRepository _orderRepository;
+
+        public Order Order { get; set; }
+
+        public OrderDetailsViewModel(int orderId)
+        {
+            _orderRepository = new OrderRepository();
+            LoadOrderAsync(orderId);
+        }
+
+        private async Task LoadOrderAsync(int orderId)
+        {
+            Order = await _orderRepository.GetOrderByOrderIdAsync(orderId);
+        }
+
+    }
+}
