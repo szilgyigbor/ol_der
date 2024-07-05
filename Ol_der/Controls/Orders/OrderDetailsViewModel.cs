09b1797181ea32fd70363c819835f894454ed6be
Gábor <szilgyigbor@gmail.com>
2024-07-05 10:48:49 +0200

    Add txt and xlsx save options
    - Replaced "Mentés fájlba" button in OrderDetailsControl.xaml with "Mentés txt fájlba" and "Mentés xlsx fájlba" buttons, linked to new commands for saving order details in .txt and .xlsx formats, respectively.
    - Updated OrderDetailsViewModel.cs by removing and adding specific `using` directives to support Excel file operations with the OfficeOpenXml package.
    - Replaced `SaveToFileCommand` with `SaveToTxtFileCommand` and `SaveToXlsxFileCommand`. Introduced `SaveToTxtFile` and `SaveToExcelFile` methods for saving order details in the respective formats, including success messages and Excel file formatting with the OfficeOpenXml package.
    - These changes enhance the application's functionality by offering more flexibility in exporting order details, catering to user preferences for text or Excel formats.

--- Ol_der/Controls/Orders/OrderDetailsViewModel.cs
+++ Ol_der/Controls/Orders/OrderDetailsViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Ol_der.Models;
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using Ol_der.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -16,13 +18,15 @@ namespace Ol_der.Controls.Orders
         private OrderRepository _orderRepository;
 
         public Order Order { get; set; }
-        public ICommand SaveToFileCommand { get; }
+        public ICommand SaveToTxtFileCommand { get; }
+        public ICommand SaveToXlsxFileCommand { get; }
 
         public OrderDetailsViewModel(int orderId)
         {
             _orderRepository = new OrderRepository();
             LoadOrderAsync(orderId);
-            SaveToFileCommand = new RelayCommand(param => SaveToFile());
+            SaveToTxtFileCommand = new RelayCommand(param => SaveToTxtFile());
+            SaveToXlsxFileCommand = new RelayCommand(param => SaveToExcelFile());
         }
 
         private async Task LoadOrderAsync(int orderId)
@@ -30,7 +34,7 @@ namespace Ol_der.Controls.Orders
             Order = await _orderRepository.GetOrderByOrderIdAsync(orderId);
         }
 
-        private void SaveToFile()
+        private void SaveToTxtFile()
         {
             try
             {
@@ -51,9 +55,54 @@ namespace Ol_der.Controls.Orders
 
                 File.WriteAllText(filePath, sb.ToString());
 
-                MessageBoxOkWindow messageBoxOkWindow = new MessageBoxOkWindow("Sikeresen elmentve a fájlba.");
+                MessageBoxOkWindow messageBoxOkWindow = new MessageBoxOkWindow("Sikeresen elmentve a txt fájlba.");
                 messageBoxOkWindow.ShowDialog();
 
+            }
+            catch (Exception ex)
+            {
+                MessageBoxOkWindow messageBoxOkWindow = new MessageBoxOkWindow($"Hiba történt a fájl mentése során: {ex.Message}");
+                messageBoxOkWindow.ShowDialog();
+            }
+        }
+
+        private void SaveToExcelFile()
+        {
+            try
+            {
+                var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                var filePath = Path.Combine(desktopPath, "orderItems.xlsx");
+
+                using (ExcelPackage package = new ExcelPackage())
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("OrderItems");
+
+                    worksheet.Cells[1, 1].Value = "Darab";
+                    worksheet.Cells[1, 2].Value = "Cikkszám";
+                    worksheet.Cells[1, 3].Value = "Terméknév";
+
+                    int row = 2;
+                    foreach (var item in Order.OrderItems)
+                    {
+                        worksheet.Cells[row, 1].Value = item.QuantityOrdered;
+                        worksheet.Cells[row, 2].Value = item.Product.ItemNumber;
+                        worksheet.Cells[row, 3].Value = item.Product.Name;
+                        row++;
+                    }
+
+                    using (var range = worksheet.Cells[1, 1, row - 1, 3])
+                    {
+                        range.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                        range.Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                    }
+
+                    worksheet.Cells[worksheet.Dimension.Address].AutoFitColumns();
+
+                    File.WriteAllBytes(filePath, package.GetAsByteArray());
+                }
+
+                MessageBoxOkWindow messageBoxOkWindow = new MessageBoxOkWindow("Sikeresen elmentve az xlsx fájlba.");
+                messageBoxOkWindow.ShowDialog();
             }
             catch (Exception ex)
             {
