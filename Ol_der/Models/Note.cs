abe46f2afead40545917bd2526be2d01bf12e399
Gábor <szilgyigbor@gmail.com>
2024-07-06 08:59:53 +0200

    Add Note model and necessary namespaces

---
+++ Ol_der/Models/Note.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ol_der.Models
+{
+    public class Note
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int NoteId { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreationDate { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public bool IsDeleted { get; set; } = false;
+    }
+}
