93caec4b02f06b884a13985252a9c69752769d7a
Gábor <szilgyigbor@gmail.com>
2024-07-06 08:59:23 +0200

    Add Warranty model to Ol_der.Models

---
+++ Ol_der/Models/Warranty.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ol_der.Models
+{
+    public class Warranty
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int WarrantyId { get; set; }
+        public string CustomerName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public int PhoneNumber { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public DateTime CreationDate { get; set; }
+        public DateTime FulfilledDate { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int SupplierId { get; set; }
+        public Supplier Supplier { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public bool IsDeleted { get; set; } = false;
+    }
+}
