08f03eb3093f6bf3ad3a6f88ac8304548d7f73ba
Gábor <szilgyigbor@gmail.com>
2024-07-06 17:46:11 +0200

    Add Warranty and Notes entities
    This commit introduces two new entities, `Warranty` and `Notes`, to the database, including their respective tables and fields. It includes the creation of the `20240706070120_AddWarrantyAndNotes` migration script for updating the database schema, adding indexes on `ProductId` and `SupplierId` for the `Warranty` table to improve query performance. Additionally, the Entity Framework Core model has been updated to configure these new entities, specifying property types, value generation strategies, and establishing relationships with other entities. This update also modifies the EF Core model snapshot to reflect the current model state, ensuring future migrations can be accurately generated.

---
+++ Ol_der/Migrations/20240706070120_AddWarriantyAndNotes.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ol_der.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWarriantyAndNotes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    NoteId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsCompleted = table.Column<bool>(type: "bit", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.NoteId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Warranty",
+                columns: table => new
+                {
+                    WarrantyId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FulfilledDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    SupplierId = table.Column<int>(type: "int", nullable: false),
+                    IsCompleted = table.Column<bool>(type: "bit", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Warranty", x => x.WarrantyId);
+                    table.ForeignKey(
+                        name: "FK_Warranty_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Warranty_Suppliers_SupplierId",
+                        column: x => x.SupplierId,
+                        principalTable: "Suppliers",
+                        principalColumn: "SupplierId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Warranty_ProductId",
+                table: "Warranty",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Warranty_SupplierId",
+                table: "Warranty",
+                column: "SupplierId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+
+            migrationBuilder.DropTable(
+                name: "Warranty");
+        }
+    }
+}
