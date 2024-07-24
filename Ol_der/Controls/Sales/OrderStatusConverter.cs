e4b1261278024baa247b2f877f8106708e179eb2
Gábor <szilgyigbor@gmail.com>
2024-07-24 18:21:38 +0200

    Replace IsOrderedToTextConverter with OrderStatusConverter
    Removed IsOrderedToTextConverter class and added OrderStatusConverter class for enhanced order status logic. Updated resource key and binding in ShowAllSaleControl.xaml to use the new converter.

---
+++ Ol_der/Controls/Sales/OrderStatusConverter.cs
@@ -0,0 +1,39 @@
+﻿using Ol_der.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Ol_der.Controls.Sales
+{
+    public class OrderStatusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is SaleItem saleItem)
+            {
+                if (!saleItem.NeedToOrder && !saleItem.IsOrdered)
+                {
+                    return "(Nem lesz rendelve)";
+                }
+                else if (!saleItem.NeedToOrder && saleItem.IsOrdered)
+                {
+                    return "(Rendelésben van)";
+                }
+                else
+                {
+                    return saleItem.IsOrdered ? "(Rendelésben van)" : "(Még nincs rendelve)";
+                }
+            }
+            return "Ismeretlen állapot";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
