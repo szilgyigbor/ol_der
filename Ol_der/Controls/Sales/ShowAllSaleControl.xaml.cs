ffa7aa395ea30c1ed99f013486a1330a2a6d5a86
Gábor <szilgyigbor@gmail.com>
2024-07-26 12:17:53 +0200

    Add SaveButton_Click event handler and SaveText method
    - Added `using` directives for necessary namespaces in `ShowAllSaleControl.xaml.cs`.
    - Implemented `SaveButton_Click` to handle button click events and save text from a `TextBlock`.
    - Introduced `SaveText` method to save text to `daySummary.txt` on the desktop and provide user feedback via message boxes.

--- Ol_der/Controls/Sales/ShowAllSaleControl.xaml.cs
+++ Ol_der/Controls/Sales/ShowAllSaleControl.xaml.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.ComponentModel;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -15,7 +16,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.CompilerServices;
 using Ol_der.Controls.Orders;
+using System.IO;
 
 namespace Ol_der.Controls.Sales
 {
@@ -31,6 +34,46 @@ namespace Ol_der.Controls.Sales
             InitializeComponent();
             _viewModel = new SaleViewModel();
             this.DataContext = _viewModel;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button && button.Parent is StackPanel panel)
+            {
+                var textBlock = panel.Children.OfType<TextBlock>().FirstOrDefault();
+                if (textBlock != null)
+                {
+                    string textToSave = textBlock.Text;
+
+                    SaveText(textToSave);
+                }
+            }
+        }
+
+        private void SaveText(string text)
+        {
+            try
+            {
+                var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                var filePath = System.IO.Path.Combine(desktopPath, "daySummary.txt");
+
+                var sb = new StringBuilder();
+                sb.AppendLine("------------------------------------------------------------------" +
+                    "-----------------------------------------------------------");
+                sb.AppendLine(text);
+
+
+                File.AppendAllText(filePath, sb.ToString());
+
+                MessageBoxOkWindow messageBoxOkWindow = new MessageBoxOkWindow("Sikeresen hozzáadva a daySummary.txt fájlhoz.");
+                messageBoxOkWindow.ShowDialog();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBoxOkWindow messageBoxOkWindow = new MessageBoxOkWindow($"Hiba történt a fájl mentése során: {ex.Message}");
+                messageBoxOkWindow.ShowDialog();
+            }
         }
 
 
