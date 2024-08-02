79076048429302ff000f7e741832d81332c38d48
Gábor <szilgyigbor@gmail.com>
2024-08-02 10:34:53 +0200

    Add NoteRepository with GetAllNoteAsync method
    Added a new NoteRepository class within the Ol_der.Controls.Notes namespace.
    Implemented the GetAllNoteAsync method to retrieve non-deleted notes asynchronously.
    Included necessary using directives for various namespaces.
    Utilized ApplicationDbContextFactory.Create() to instantiate the database context.

---
+++ Ol_der/Controls/Notes/NoteRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ol_der.Data;
+using Ol_der.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ol_der.Controls.Notes
+{
+    internal class NoteRepository
+    {
+        public async Task<List<Note>> GetAllNoteAsync()
+        {
+            using (var context = ApplicationDbContextFactory.Create())
+            {
+                return await context.Notes.Where(n => !n.IsDeleted).ToListAsync();
+            }
+        }
+    }
+}
