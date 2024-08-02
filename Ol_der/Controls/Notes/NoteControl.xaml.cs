6459d9809e895937efb05350889f042cf7272552
Gábor <szilgyigbor@gmail.com>
2024-08-02 10:35:10 +0200

    Add NoteControl user control with initial ViewModel
    Added a new `UserControl` named `NoteControl` in `NoteControl.xaml` with a grid layout. The grid includes a header, a main content area divided into two columns, and a `GridSplitter` for resizing. Introduced `NoteControl.xaml.cs` to initialize the control and set its data context to `NoteControlViewModel`. Created an empty `NoteControlViewModel` class in `NoteControlViewModel.cs`.

---
+++ Ol_der/Controls/Notes/NoteControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Ol_der.Controls.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ol_der.Controls.Notes
+{
+    /// <summary>
+    /// Interaction logic for NoteControl.xaml
+    /// </summary>
+    public partial class NoteControl : UserControl
+    {
+        private NoteControlViewModel _viewModel;
+
+        public NoteControl()
+        {
+            InitializeComponent();
+            _viewModel = new NoteControlViewModel();
+            DataContext = _viewModel;
+        }
+    }
+}
