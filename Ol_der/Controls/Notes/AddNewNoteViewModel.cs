5a5e7508a75445cc1c7a6c066fe7c0f6951546cb
Gábor <szilgyigbor@gmail.com>
2024-08-03 17:15:47 +0200

    Add AddNewNoteViewModel with save functionality
    Added a new ViewModel class `AddNewNoteViewModel` to the `Ol_der.Controls.Notes` namespace. Implemented the `INotifyPropertyChanged` interface to support property change notifications. Introduced a private field `_note` and a public property `Note` to hold the note data. Added a `SaveNoteCommand` property of type `ICommand` to handle the save note action. Created a constructor that initializes the `NoteRepository`, sets up the `SaveNoteCommand` with a `RelayCommand`, and initializes the `Note` property. Implemented the `SaveNote` method to set the creation date of the note, save it using the `NoteRepository`, and display a confirmation message box. Added the `OnPropertyChanged` method to raise the `PropertyChanged` event when a property value changes.

---
+++ Ol_der/Controls/Notes/AddNewNoteViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Ol_der.Models;
+using Ol_der.Controls.Orders;
+using Ol_der.Controls.Notes;
+
+namespace Ol_der.Controls.Notes
+{
+    public class AddNewNoteViewModel : INotifyPropertyChanged
+    {
+        private readonly NoteRepository _noteRepository;
+
+        private Note _note;
+        public Note Note
+        {
+            get => _note;
+            set
+            {
+                _note = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand SaveNoteCommand { get; }
+
+        public AddNewNoteViewModel()
+        {
+            _noteRepository = new NoteRepository();
+            SaveNoteCommand = new RelayCommand(async () => await SaveNote());
+            Note = new Note();
+        }
+
+        public async Task SaveNote()
+        {
+            Note.CreationDate = DateTime.Now;
+
+            await _noteRepository.AddNewNote(Note);
+
+            MessageBoxOkWindow messageBoxWindow = new("Jegyzet sikeresen mentve");
+            messageBoxWindow.ShowDialog();
+        }
+
+        
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
