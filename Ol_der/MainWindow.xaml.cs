ece9be3a6ae1824a98d261b81649016bdfb6a4b7
Gábor <szilgyigbor@gmail.com>
2024-08-03 17:16:14 +0200

    Add "Jegyzetek" button and handle its click event
    A new "Jegyzetek" button has been added to the MainWindow.xaml file.
    The MainWindow.xaml.cs file has been updated to include a new using
    directive for the Ol_der.Controls.Notes namespace. Additionally, a
    new case for the "btnNote" button has been added to the
    Menu_Button_Click event handler to display a NoteControl in the
    ContentArea when the button is clicked.

--- Ol_der/MainWindow.xaml.cs
+++ Ol_der/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using Ol_der.Controls.Sales;
 using Ol_der.Controls.Suppliers;
 using Ol_der.Controls.SalePackages;
 using Ol_der.Controls.Orders;
+using Ol_der.Controls.Notes;
 
 
 namespace Ol_der
@@ -48,6 +49,9 @@ namespace Ol_der
                     break;
                 case "btnOrder":
                     ContentArea.Content = new OrderControl();
+                    break;
+                case "btnNote":
+                    ContentArea.Content = new NoteControl();
                     break;
             }
         }
