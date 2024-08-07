525a45d0a148cfe209a9c78d8b8f7919e9ded903
Gábor <szilgyigbor@gmail.com>
2024-08-07 09:06:09 +0200

    Refactor NoteViewModel: remove commands and properties
    Significantly refactored the NoteViewModel class:
    - Removed INotifyPropertyChanged implementation.
    - Deleted CurrentViewModel property and its backing field.
    - Removed commands: ShowAllNotesCommand, AddNoteCommand, ModifyNoteCommand, DeleteNoteCommand.
    - Simplified constructor by removing command initialization and CurrentViewModel setting.
    - Removed methods: ShowAllNotes, AddNote, ModifyNote, DeleteNote.
    - Removed OnPropertyChanged method and PropertyChanged event.
    - Added new using directives: System.Diagnostics, System.Linq, System.Runtime.CompilerServices, Ol_der.Controls.Orders, Ol_der.Models.

--- Ol_der/Controls/Notes/NoteViewModel.cs
+++ Ol_der/Controls/Notes/NoteViewModel.cs
@@ -1,67 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Ol_der.Controls.Orders;
+using Ol_der.Models;
 
 namespace Ol_der.Controls.Notes
 {
-    public class NoteViewModel : INotifyPropertyChanged
+    public class NoteViewModel
     {
-        private object _currentViewModel;
-
-        public object CurrentViewModel
-        {
-            get => _currentViewModel;
-            set
-            {
-                _currentViewModel = value;
-                OnPropertyChanged(nameof(CurrentViewModel));
-            }
-        }
-
-        public ICommand ShowAllNotesCommand { get; }
-        public ICommand AddNoteCommand { get; }
-        public ICommand ModifyNoteCommand { get; }
-        public ICommand DeleteNoteCommand { get; }
-
         public NoteViewModel()
         {
-            ShowAllNotesCommand = new RelayCommand(ShowAllNotes);
-            AddNoteCommand = new RelayCommand(AddNote);
-            ModifyNoteCommand = new RelayCommand(ModifyNote);
-            DeleteNoteCommand = new RelayCommand(DeleteNote);
-
-            CurrentViewModel = new ShowAllNoteViewModel();
         }
 
-        private void ShowAllNotes()
-        {
-            CurrentViewModel = new ShowAllNoteViewModel();
-        }
-
-        private void AddNote()
-        {
-            CurrentViewModel = new AddNewNoteViewModel();
-        }
-
-        private void ModifyNote()
-        {
-            // Logika a jegyzet módosítására
-        }
-
-        private void DeleteNote()
-        {
-            // Logika a jegyzet törlésére
-        }
-
-        protected virtual void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
