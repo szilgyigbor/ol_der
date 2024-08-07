be796f89c493b45b9620c952be9e89609fbb03e3
Gábor <szilgyigbor@gmail.com>
2024-08-07 13:27:19 +0200

    Add NoteDetailControl to display note details
    Introduced a new `NoteDetailControl` class within the `Ol_der.Controls.Notes` namespace. This `UserControl` is designed to display the details of a note.
    
    - Added a private `_viewModel` field of type `NoteDetailViewModel`.
    - Implemented a constructor that initializes the control and sets the `DataContext` to the `_viewModel` instantiated with the provided `Note` object.
    - Included necessary `using` directives for the control's functionality.

---
+++ Ol_der/Controls/Notes/NoteDetailControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Ol_der.Models;
+
+namespace Ol_der.Controls.Notes
+{
+    /// <summary>
+    /// Interaction logic for NoteDetailControl.xaml
+    /// </summary>
+    public partial class NoteDetailControl : UserControl
+    {
+        private NoteDetailViewModel _viewModel;
+        public NoteDetailControl(Note Note)
+        {
+            InitializeComponent();
+            _viewModel = new NoteDetailViewModel(Note);
+            this.DataContext = _viewModel;
+        }
+    }
+}
