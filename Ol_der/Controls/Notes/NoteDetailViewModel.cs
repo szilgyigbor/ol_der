07f8f93e35bdd87bf4265b9c60a6de00778c6fcc
Gábor <szilgyigbor@gmail.com>
2024-08-07 13:27:30 +0200

    Add NoteDetailViewModel with INotifyPropertyChanged
    Introduce NoteDetailViewModel in Ol_der.Controls.Notes namespace.
    This ViewModel supports property change notifications via the
    INotifyPropertyChanged interface, facilitating data binding in
    MVVM architecture. It includes a Note property, a constructor
    for initialization, and an OnPropertyChanged method to raise
    property change events.

---
+++ Ol_der/Controls/Notes/NoteDetailViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Ol_der.Models;
+
+namespace Ol_der.Controls.Notes
+{
+    internal class NoteDetailViewModel : INotifyPropertyChanged
+    {
+        private Note _note;
+        public Note Note
+        {
+            get => _note;
+            set
+            {
+                _note = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public NoteDetailViewModel(Note note)
+        {
+            _note = note;
+        }
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
