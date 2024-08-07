2ad85150e82e6695d13afdc348d825deba47ad67
Gábor <szilgyigbor@gmail.com>
2024-08-07 09:05:48 +0200

    Enhance AddNewNoteViewModel for note creation and updates
    Updated AddNewNoteViewModel to handle both new note creation and
    existing note updates. Added a private boolean field `_isNewNote`
    to track the note state. Modified the constructor to accept a
    `Note` object (`noteToUpdate`). Introduced `CheckNote` method to
    set `_isNewNote` and initialize the `Note` property. Updated
    `SaveNote` method to check `_isNewNote` and perform appropriate
    actions, including setting creation date, adding new notes, or
    updating existing notes. Display different success messages based
    on the action performed.

--- Ol_der/Controls/Notes/AddNewNoteViewModel.cs
+++ Ol_der/Controls/Notes/AddNewNoteViewModel.cs
@@ -11,6 +11,7 @@ namespace Ol_der.Controls.Notes
     {
         private readonly NoteRepository _noteRepository;
 
+        private bool _isNewNote;
         private Note _note;
         public Note Note
         {
@@ -25,24 +26,48 @@ namespace Ol_der.Controls.Notes
         public event PropertyChangedEventHandler PropertyChanged;
         public ICommand SaveNoteCommand { get; }
 
-        public AddNewNoteViewModel()
+        public AddNewNoteViewModel(Note noteToUpdate)
         {
             _noteRepository = new NoteRepository();
             SaveNoteCommand = new RelayCommand(async () => await SaveNote());
-            Note = new Note();
+            CheckNote(noteToUpdate);
+        }
+
+        public void CheckNote(Note noteToUpdate)
+        {
+            if (noteToUpdate == null)
+            {
+                Note = new Note();
+                _isNewNote = true;
+            }
+
+            else
+            {
+                Note = noteToUpdate;
+                _isNewNote = false;
+            }
         }
 
         public async Task SaveNote()
         {
-            Note.CreationDate = DateTime.Now;
+            if (_isNewNote)
+            {
+                Note.CreationDate = DateTime.Now;
 
-            await _noteRepository.AddNewNote(Note);
+                await _noteRepository.AddNewNote(Note);
 
-            MessageBoxOkWindow messageBoxWindow = new("Jegyzet sikeresen mentve");
-            messageBoxWindow.ShowDialog();
+                MessageBoxOkWindow messageBoxWindow = new("Jegyzet sikeresen mentve");
+                messageBoxWindow.ShowDialog();
+            }
+            else 
+            {
+                await _noteRepository.UpdateNote(Note);
+
+                MessageBoxOkWindow messageBoxWindow = new("Jegyzet sikeresen módosítva");
+                messageBoxWindow.ShowDialog();
+            }
+            
         }
-
-        
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
