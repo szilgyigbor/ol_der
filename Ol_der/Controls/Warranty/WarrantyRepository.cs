1c1a7333ca193a0d564264648f2512f6f24611f4
Gábor <szilgyigbor@gmail.com>
2024-08-23 10:22:21 +0200

    Add WarrantyRepository class and update using directives
    Added several `using` directives for namespaces: `System`, `System.Collections.Generic`, `System.Linq`, `System.Text`, `System.Threading.Tasks`, `Ol_der.Data`, and `Ol_der.Models`. Introduced a new namespace `Ol_der.Controls.Warranty` and created an internal class `WarrantyRepository` within it.

---
+++ Ol_der/Controls/Warranty/WarrantyRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ol_der.Data;
+using Ol_der.Models;
+
+namespace Ol_der.Controls.Warranty
+{
+    internal class WarrantyRepository
+    {
+    }
+}
