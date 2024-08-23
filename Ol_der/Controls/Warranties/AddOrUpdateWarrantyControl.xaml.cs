16d86d381153e8e965c375acbb8a472d66506044
Gábor <szilgyigbor@gmail.com>
2024-08-23 20:42:42 +0200

    Add AddOrUpdateWarrantyControl for managing warranties
    Introduce AddOrUpdateWarrantyControl class in Ol_der.Controls.Warranties namespace.
    - Add private field _viewModel of type AddOrUpdateWarrantyViewModel.
    - Implement constructor with optional Warranty parameter.
    - Initialize component and set DataContext to new view model instance.
    - Include necessary using directives for required namespaces.

---
+++ Ol_der/Controls/Warranties/AddOrUpdateWarrantyControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Ol_der.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ol_der.Controls.Warranties
+{
+    /// <summary>
+    /// Interaction logic for AddOrUpdateWarrantyControl.xaml
+    /// </summary>
+    public partial class AddOrUpdateWarrantyControl : UserControl
+    {
+        private AddOrUpdateWarrantyViewModel _viewModel;
+        public AddOrUpdateWarrantyControl(Warranty warrantyToUpdate = null)
+        {
+            InitializeComponent();
+            _viewModel = new(warrantyToUpdate);
+            this.DataContext = _viewModel;
+        }
+    }
+}
