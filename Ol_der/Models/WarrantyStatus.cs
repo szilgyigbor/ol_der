e160ee06507ab76cd84bafb8ce13380d1e21f281
Gábor <szilgyigbor@gmail.com>
2024-08-23 09:32:08 +0200

    Add WarrantyStatus model to track warranty statuses
    Introduced a new class `WarrantyStatus` in the `Ol_der.Models` namespace.
    This class includes properties for `WarrantyStatusId`, `WarrantyId`,
    `Warranty`, `StatusDescription`, and `StatusDate`. The `StatusDescription`
    is initialized to an empty string, and `StatusDate` is initialized to the
    current date and time. Added necessary `using` directives for required
    namespaces.

---
+++ Ol_der/Models/WarrantyStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ol_der.Models
+{
+    public class WarrantyStatus
+    {
+        public int WarrantyStatusId { get; set; }
+        public int WarrantyId { get; set; }
+        public Warranty Warranty { get; set; }
+        public string StatusDescription { get; set; } = string.Empty;
+        public DateTime StatusDate { get; set; } = DateTime.Now;
+    }
+}
