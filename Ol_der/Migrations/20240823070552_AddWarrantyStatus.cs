8a1133b038d62e8de8b00cd63edfcfeb53b0067f
Gábor <szilgyigbor@gmail.com>
2024-08-23 09:33:06 +0200

    Add WarrantyStatus entity and related migration
    Introduce a new WarrantyStatus entity to the database schema.
    Add migration file 20240823070552_AddWarrantyStatus.Designer.cs.
    Define properties: WarrantyStatusId, WarrantyId, StatusDescription, and StatusDate.
    Establish foreign key relationship with Warranty entity.
    Update ApplicationDbContextModelSnapshot.cs to include WarrantyStatus.
    Configure primary key, indexes, and navigation properties.

---
+++ Ol_der/Migrations/20240823070552_AddWarrantyStatus.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ol_der.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWarrantyStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WarrantyStatus",
+                columns: table => new
+                {
+                    WarrantyStatusId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WarrantyId = table.Column<int>(type: "int", nullable: false),
+                    StatusDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StatusDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WarrantyStatus", x => x.WarrantyStatusId);
+                    table.ForeignKey(
+                        name: "FK_WarrantyStatus_Warranty_WarrantyId",
+                        column: x => x.WarrantyId,
+                        principalTable: "Warranty",
+                        principalColumn: "WarrantyId",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WarrantyStatus_WarrantyId",
+                table: "WarrantyStatus",
+                column: "WarrantyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WarrantyStatus");
+        }
+    }
+}
