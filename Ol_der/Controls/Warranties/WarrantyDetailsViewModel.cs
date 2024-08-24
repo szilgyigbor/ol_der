ba4f870680a14b449b56a3123c8b49a0bc3d4d27
Gábor <szilgyigbor@gmail.com>
2024-08-24 22:29:06 +0200

    Add WarrantyDetailsViewModel with property change support
    A new WarrantyDetailsViewModel class has been added to the Ol_der.Controls.Warranties namespace. This class implements the INotifyPropertyChanged interface to support property change notifications. It includes private fields for WarrantyRepository and Warranty, and a public property Warranty that sorts the WarrantyStatuses collection by StatusDate in descending order when set. The PropertyChanged event is declared to notify clients of property value changes. A constructor initializes the _warrantyRepository and sets the Warranty property. A protected OnPropertyChanged method raises the PropertyChanged event.

---
+++ Ol_der/Controls/Warranties/WarrantyDetailsViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Ol_der.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ol_der.Controls.Warranties
+{
+    class WarrantyDetailsViewModel : INotifyPropertyChanged
+    {
+        private WarrantyRepository _warrantyRepository;
+        private Warranty _warranty;
+
+        public Warranty Warranty
+        {
+            get { return _warranty; }
+            set
+            {
+                _warranty = value;
+
+                var sortedStatuses = new ObservableCollection<WarrantyStatus>(_warranty.WarrantyStatuses.OrderByDescending(s => s.StatusDate));
+                _warranty.WarrantyStatuses = sortedStatuses;
+
+                OnPropertyChanged(nameof(Warranty));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public WarrantyDetailsViewModel(Warranty warrantyDetails)
+        {
+            _warrantyRepository = new();
+            Warranty = warrantyDetails;
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
