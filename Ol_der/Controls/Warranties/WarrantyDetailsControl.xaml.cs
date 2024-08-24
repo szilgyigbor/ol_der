1987ab8b4aef85e51967614398e743fae2e2a4de
Gábor <szilgyigbor@gmail.com>
2024-08-24 22:29:39 +0200

    Add WarrantyDetailsControl for displaying warranty details
    Introduce WarrantyDetailsControl in Ol_der.Controls.Warranties.
    This UserControl initializes with a Warranty object and sets up
    a WarrantyDetailsViewModel for data binding. The control's
    DataContext is set to the view model to manage warranty details.

---
+++ Ol_der/Controls/Warranties/WarrantyDetailsControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Ol_der.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ol_der.Controls.Warranties
+{
+    /// <summary>
+    /// Interaction logic for WarrantyDetailsControl.xaml
+    /// </summary>
+    public partial class WarrantyDetailsControl : UserControl
+    {
+        private WarrantyDetailsViewModel _viewModel;
+
+        public WarrantyDetailsControl(Warranty warrantyToDetails)
+        {
+            InitializeComponent();
+            _viewModel = new(warrantyToDetails);
+            this.DataContext = _viewModel;
+        }
+    }
+}
