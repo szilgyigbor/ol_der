0f17c1c6298a8408d674b433efd8214b7da1ce28
Gábor <szilgyigbor@gmail.com>
2024-08-28 16:41:14 +0200

    Refactor CustomerOrderDetailViewModel for MVVM compliance
    Implemented INotifyPropertyChanged in CustomerOrderDetailViewModel.
    Added private fields for repository and order.
    Added public CustomerOrder property with sorting and notification.
    Added PropertyChanged event and OnPropertyChanged method.
    Moved class to Ol_der.Models namespace and changed access to public.

--- Ol_der/Controls/CustomerOrders/CustomerOrderDetailViewModel.cs
+++ Ol_der/Controls/CustomerOrders/CustomerOrderDetailViewModel.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using Ol_der.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Ol_der.Controls.CustomerOrders
 {
-    internal class CustomerOrderDetailViewModel
+    class CustomerOrderDetailViewModel : INotifyPropertyChanged
     {
+        private CustomerOrderRepository _customerOrderRepository;
+        private CustomerOrder _customerOrder;
+        public CustomerOrder CustomerOrder
+        {
+            get { return _customerOrder; }
+            set
+            {
+                _customerOrder = value;
+
+                var sortedStatuses = new ObservableCollection<CustomerOrderStatus>(_customerOrder.CustomerOrderStatuses.OrderByDescending(s => s.StatusDate));
+                _customerOrder.CustomerOrderStatuses = sortedStatuses;
+
+                OnPropertyChanged(nameof(CustomerOrder));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public CustomerOrderDetailViewModel(CustomerOrder customerOrderDetails)
+        {
+            _customerOrderRepository = new CustomerOrderRepository();
+            CustomerOrder = customerOrderDetails;
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
