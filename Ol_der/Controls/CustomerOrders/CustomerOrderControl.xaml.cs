04aa1e416c285748da512b63f1bb707b6e2644dc
Gábor <szilgyigbor@gmail.com>
2024-08-28 10:49:18 +0200

    Add CustomerOrderControl for managing customer orders
    Introduce a new CustomerOrderControl user control in the
    Ol_der.Controls.CustomerOrders namespace. The XAML layout
    includes a Grid with a TextBlock for the title and a
    ContentControl for dynamic content, along with buttons for
    various actions. The code-behind initializes the control,
    sets up the initial view, and implements the method to
    display all customer orders.

---
+++ Ol_der/Controls/CustomerOrders/CustomerOrderControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ol_der.Controls.CustomerOrders
+{
+    /// <summary>
+    /// Interaction logic for CustomerOrderControl.xaml
+    /// </summary>
+    public partial class CustomerOrderControl : UserControl
+    {
+        private CustomerOrderRepository _customerOrderRepository;
+        private ShowAllCustomerOrderControl _showAllCustomerOrderControl;
+
+        public CustomerOrderControl()
+        {
+            InitializeComponent();
+            ShowAllCustomerOrder();
+            _customerOrderRepository = new();
+        }
+
+        public void ShowAllCustomerOrder(int customerOrderNumber = 100)
+        {
+            _showAllCustomerOrderControl = new ShowAllCustomerOrderControl(customerOrderNumber);
+            ContentArea.Content = _showAllCustomerOrderControl;
+        }
+    }
+}
