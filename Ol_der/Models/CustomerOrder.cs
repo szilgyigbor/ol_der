21c2e0e72f698f56d617c926ec23266e5e701011
Gábor <szilgyigbor@gmail.com>
2024-08-28 09:47:36 +0200

    Add CustomerOrder and CustomerOrderStatus classes
    Introduce two new classes, CustomerOrder and CustomerOrderStatus,
    in the Ol_der.Models namespace. CustomerOrder includes properties
    for order details and a collection of statuses. CustomerOrderStatus
    includes properties for status details and links to CustomerOrder.

---
+++ Ol_der/Models/CustomerOrder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ol_der.Models
+{
+    public class CustomerOrder
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CustomerOrderId { get; set; }
+        public string CustomerName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string OrderDescription { get; set; } = string.Empty;
+        public DateTime CreationDate { get; set; }
+        public DateTime FulfilledDate { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public bool IsDeleted { get; set; } = false;
+
+        public ObservableCollection<CustomerOrderStatus> CustomerOrderStatuses { get; set; } = new ObservableCollection<CustomerOrderStatus>();
+    }
+}
---
+++ Ol_der/Models/CustomerOrderStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ol_der.Models
+{
+    public class CustomerOrderStatus
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CustomerOrderStatusId { get; set; }
+        public int CustomerOrderId { get; set; }
+        public CustomerOrder CustomerOrder { get; set; }
+        public string StatusDescription { get; set; } = string.Empty;
+        public DateTime StatusDate { get; set; } = DateTime.Now;
+    }
+}
