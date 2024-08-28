cb3bf2ec5bfa8b662794c831a4b094bf5a1e2dc1
Gábor <szilgyigbor@gmail.com>
2024-08-28 09:48:08 +0200

    Add CustomerOrders and CustomerOrderStatuses tables
    Introduce migration `20240828074552_AddCustomerOrders` to add
    `CustomerOrders` and `CustomerOrderStatuses` tables. Define
    columns for both tables and establish foreign key relationships.
    Update `ApplicationDbContextModelSnapshot` to reflect these
    new entities and their relationships. Configure navigation
    properties for `CustomerOrder` and `CustomerOrderStatus`.

---
+++ Ol_der/Migrations/20240828074552_AddCustomerOrders.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ol_der.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCustomerOrders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CustomerOrders",
+                columns: table => new
+                {
+                    CustomerOrderId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrderDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FulfilledDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsCompleted = table.Column<bool>(type: "bit", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerOrders", x => x.CustomerOrderId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomerOrderStatuses",
+                columns: table => new
+                {
+                    CustomerOrderStatusId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerOrderId = table.Column<int>(type: "int", nullable: false),
+                    StatusDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StatusDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerOrderStatuses", x => x.CustomerOrderStatusId);
+                    table.ForeignKey(
+                        name: "FK_CustomerOrderStatuses_CustomerOrders_CustomerOrderId",
+                        column: x => x.CustomerOrderId,
+                        principalTable: "CustomerOrders",
+                        principalColumn: "CustomerOrderId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerOrderStatuses_CustomerOrderId",
+                table: "CustomerOrderStatuses",
+                column: "CustomerOrderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomerOrderStatuses");
+
+            migrationBuilder.DropTable(
+                name: "CustomerOrders");
+        }
+    }
+}
