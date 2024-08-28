adee5851bcb26154112579ec32f4c600d26f998c
Gábor <szilgyigbor@gmail.com>
2024-08-28 10:50:49 +0200

    Add ShowAllCustomerOrderControl and ViewModel
    Added ShowAllCustomerOrderControl with XAML and code-behind.
    Introduced ShowAllCustomerOrderViewModel implementing INotifyPropertyChanged.
    Implemented async loading of customer orders in ViewModel.

---
+++ Ol_der/Controls/CustomerOrders/ShowAllCustomerOrderControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ol_der.Controls.CustomerOrders
+{
+    /// <summary>
+    /// Interaction logic for ShowAllCustomerOrderControl.xaml
+    /// </summary>
+    public partial class ShowAllCustomerOrderControl : UserControl
+    {
+        private ShowAllCustomerOrderViewModel _viewModel;
+        public ShowAllCustomerOrderControl(int limit)
+        {
+            InitializeComponent();
+            _viewModel = new(limit);
+            this.DataContext = _viewModel;
+        }
+    }
+}
---
+++ Ol_der/Controls/CustomerOrders/ShowAllCustomerOrderViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Ol_der.Data;
+using Ol_der.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ol_der.Controls.CustomerOrders
+{
+    public class ShowAllCustomerOrderViewModel : INotifyPropertyChanged
+    {
+        private CustomerOrderRepository _customerOrderRepository;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private ObservableCollection<CustomerOrder> _customerOrders;
+        public ObservableCollection<CustomerOrder> CustomerOrders
+        {
+            get { return _customerOrders; }
+            set
+            {
+                _customerOrders = value;
+                OnPropertyChanged(nameof(CustomerOrders));
+            }
+        }
+
+        public ShowAllCustomerOrderViewModel(int limit)
+        {
+            _customerOrderRepository = new CustomerOrderRepository();
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public async Task LoadCustomerOrdersAsync(int limit)
+        {
+            var customerOrders = await _customerOrderRepository.GetLimitedNumberOfCustomerOrderAsync(limit);
+            CustomerOrders = new ObservableCollection<CustomerOrder>(customerOrders);
+        }
+    }
+}
