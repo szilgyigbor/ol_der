7ec3a6df7cece856ad0a2836054682a6f902f850
Gábor <szilgyigbor@gmail.com>
2024-08-28 10:49:35 +0200

    Add CustomerOrderRepository with limited order retrieval
    A new `CustomerOrderRepository` class has been added to the
    `Ol_der.Controls.CustomerOrders` namespace. This class includes
    a method `GetLimitedNumberOfCustomerOrderAsync` which retrieves
    a limited number of customer orders from the database using
    Entity Framework Core. The method queries the `CustomerOrders`
    table, includes related `CustomerOrderStatuses`, and orders the
    results by `CreationDate` in descending order. It takes an
    integer parameter `limit` to specify the maximum number of
    customer orders to retrieve. The `ApplicationDbContextFactory.
    Create()` method is used to create a new instance of the
    database context.

---
+++ Ol_der/Controls/CustomerOrders/CustomerOrderRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ol_der.Data;
+using Ol_der.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ol_der.Controls.CustomerOrders
+{
+    public class CustomerOrderRepository
+    {
+        public async Task<List<CustomerOrder>> GetLimitedNumberOfCustomerOrderAsync(int limit)
+        {
+            using (var context = ApplicationDbContextFactory.Create())
+            {
+                return await context.CustomerOrders
+                    .Include(c => c.CustomerOrderStatuses)
+                    .OrderByDescending(w => w.CreationDate)
+                    .Take(limit)
+                    .ToListAsync();
+            }
+        }
+
+
+    }
+}
