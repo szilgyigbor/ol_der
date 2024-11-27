e6cd39f97ecc004bf674f96db3218b153e662122
Gábor <szilgyigbor@gmail.com>
2024-11-27 16:47:40 +0100

    Upgrade SetDateFilter control to work.

--- Ol_der/Controls/DateFilter/SetDateToFilter.xaml.cs
+++ Ol_der/Controls/DateFilter/SetDateToFilter.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ol_der.Controls.Orders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,12 +20,15 @@ namespace Ol_der.Controls.DateFilter
     /// </summary>
     public partial class SetDateToFilter : Window
     {
-        public DateTime? StartDate { get; set; }
-        public DateTime? EndDate { get; set; }
+        private DateTime? StartDate { get; set; }
+        private DateTime? EndDate { get; set; }
+
+        public List<DateTime> dateTimes { get; private set; }
 
         public SetDateToFilter()
         {
             InitializeComponent();
+            dateTimes = new List<DateTime>();
         }
 
         private void StartDatePicker_SelectedDateChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
@@ -49,6 +53,28 @@ namespace Ol_der.Controls.DateFilter
             {
                 SelectedRangeTextBlock.Text = "Nincs teljes intervallum kiválasztva.";
             }
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (StartDate == null || EndDate == null)
+            {
+                MessageBoxOkWindow messageBoxOkWindow = new MessageBoxOkWindow("Kell kezdő és vég dátum is");
+                messageBoxOkWindow.ShowDialog();
+            }
+
+            else 
+            {
+                dateTimes.Clear();
+                dateTimes.Add(StartDate.Value);
+                dateTimes.Add(EndDate.Value);
+                DialogResult = true;
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
         }
     }
 }
