0aed83ffdf4bc13d5443d38ab1099e24aeb624b3
Gábor <szilgyigbor@gmail.com>
2025-01-19 12:05:42 +0100

    Add a try catch block to db connect at app start

--- Ol_der/App.xaml.cs
+++ Ol_der/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Ol_der.Controls.Orders;
 using Ol_der.Data;
+using System;
 using System.Configuration;
 using System.Data;
 using System.IO;
@@ -17,20 +19,34 @@ namespace Ol_der
 
         protected override void OnStartup(StartupEventArgs e)
         {
+            base.OnStartup(e);
+
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
             Configuration = builder.Build();
-             
+
             var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
             optionsBuilder.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
 
-            using (var context = new ApplicationDbContext(optionsBuilder.Options))
+            Task.Run(() =>
             {
-                context.Database.EnsureCreated();
-            }
-
-            base.OnStartup(e);
+                try
+                {
+                    using (var context = new ApplicationDbContext(optionsBuilder.Options))
+                    {
+                        context.Database.EnsureCreated();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        MessageBoxOkWindow messageBoxOkWindow = new MessageBoxOkWindow($"Nem sikerült csatlakozni az adatbázishoz");
+                        messageBoxOkWindow.ShowDialog();
+                    });
+                }
+            });
         }
     }
 
