ae791545991200171c155b01244fe4a6a8bed24a
Gábor <szilgyigbor@gmail.com>
2025-01-19 12:05:09 +0100

    Add db connection check to DbContextFactory

--- Ol_der/Data/ApplicationDbContextFactory.cs
+++ Ol_der/Data/ApplicationDbContextFactory.cs
@@ -2,6 +2,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System.IO;
+using System.Windows;
+using Ol_der.Controls.Orders;
 
 namespace Ol_der.Data
 {
@@ -19,6 +21,7 @@ namespace Ol_der.Data
 
         private static ApplicationDbContext CreateApplicationDbContext()
         {
+           
             var configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json")
@@ -27,7 +30,16 @@ namespace Ol_der.Data
             var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
             builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
 
-            return new ApplicationDbContext(builder.Options);
+            var context = new ApplicationDbContext(builder.Options);
+
+            if (!context.Database.CanConnect())
+            {
+                MessageBoxOkWindow messageBoxOkWindow0 = new MessageBoxOkWindow("Nincs adatbázis kapcsolat");
+                messageBoxOkWindow0.ShowDialog();
+            }
+
+            return context;
+
         }
     }
 }
