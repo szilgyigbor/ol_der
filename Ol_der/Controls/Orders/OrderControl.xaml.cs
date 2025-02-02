17388417513e26c871652ffc7c3a6f80f65e4f9b
Gábor <szilgyigbor@gmail.com>
2025-02-02 12:40:49 +0100

    Add "Duplikáció" button to OrderControl to use ExcelProcessor

--- Ol_der/Controls/Orders/OrderControl.xaml.cs
+++ Ol_der/Controls/Orders/OrderControl.xaml.cs
@@ -1,20 +1,7 @@
-﻿using Ol_der.Controls.Sales;
-using Ol_der.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Ol_der.Models;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Media.Media3D;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Ol_der.Controls.Orders
 {
@@ -229,6 +216,12 @@ namespace Ol_der.Controls.Orders
             }
 
 
+        }
+
+        private async void RemoveDuplicates_Click(object sender, RoutedEventArgs e)
+        {
+            ExcelProcessor processor = new ExcelProcessor();
+            processor.ProcessExcelFile();
         }
     }
 }
