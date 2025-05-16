3a85b0a5f8781b1f99b7d3cd4059553dbbb28a48
Gábor <szilgyigbor@gmail.com>
2025-05-16 10:59:41 +0200

    Rename OrderStatusConverter to OrderStatusTextConverter

--- Ol_der/Controls/Sales/OrderStatusTextConverter.cs
+++ Ol_der/Controls/Sales/OrderStatusTextConverter.cs
@@ -9,7 +9,7 @@ using System.Windows.Data;
 
 namespace Ol_der.Controls.Sales
 {
-    public class OrderStatusConverter : IValueConverter
+    public class OrderStatusTextConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
