088ba4286ef58b99569def41662421b619f788ca
Gábor <szilgyigbor@gmail.com>
2025-05-16 10:59:54 +0200

    Add OrderStatusIconConverter

---
+++ Ol_der/Controls/Sales/OrderStatusIconConverter.cs
@@ -0,0 +1,33 @@
+﻿using Ol_der.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Ol_der.Controls.Sales
+{
+    public class OrderStatusIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is SaleItem saleItem)
+            {
+                if (!saleItem.NeedToOrder && !saleItem.IsOrdered)
+                    return "/Icons/rejected.png";
+                else if (!saleItem.NeedToOrder && saleItem.IsOrdered)
+                    return "/Icons/approved.png";
+                else
+                    return saleItem.IsOrdered ? "/Icons/approved.png" : "/Icons/time.png";
+            }
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
