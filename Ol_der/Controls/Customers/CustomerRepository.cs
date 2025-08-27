9b77a8109b845099324d0f36d6f0718ec6b7f1cd
Gábor <szilgyigbor@gmail.com>
2025-08-27 16:10:34 +0200

    Make CustomerRepository public and add AddCustomerAsync
    Updated the `CustomerRepository` class to be public, enabling access from other assemblies. Introduced a new asynchronous method `AddCustomerAsync` for adding `Customer` objects to the database using `ApplicationDbContext`. Added necessary namespaces for `Ol_der.Data` and `Ol_der.Models`.

--- Ol_der/Controls/Customers/CustomerRepository.cs
+++ Ol_der/Controls/Customers/CustomerRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Ol_der.Data;
+using Ol_der.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,17 @@ using System.Threading.Tasks;
 
 namespace Ol_der.Controls.Customers
 {
-    class CustomerRepository
+    public class CustomerRepository
     {
+        public async Task AddCustomerAsync(Customer newCustomer)
+        {
+            using (var context = ApplicationDbContextFactory.Create())
+            {
+                context.Customers.Add(newCustomer);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        
     }
 }
