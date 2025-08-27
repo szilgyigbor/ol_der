1b8982ae32630f8553bb23d7f972659b636dc2e2
Gábor <szilgyigbor@gmail.com>
2025-08-27 09:59:29 +0200

    Add ShowAllCustomerControl and ViewModel
    Introduces a new user control `ShowAllCustomerControl` with a basic layout in XAML. The code-behind initializes a `ShowAllCustomerViewModel` as the data context. A new `ShowAllCustomerViewModel` class is also created, currently without properties or methods.

---
+++ Ol_der/Controls/Customers/ShowAllCustomerControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ol_der.Controls.Customers
+{
+    /// <summary>
+    /// Interaction logic for ShowAllCustomerControl.xaml
+    /// </summary>
+    public partial class ShowAllCustomerControl : UserControl
+    {
+        private ShowAllCustomerViewModel _viewModel;
+        public ShowAllCustomerControl()
+        {
+            InitializeComponent();
+            _viewModel = new ShowAllCustomerViewModel();
+            this.DataContext = _viewModel;
+        }
+    }
+}
