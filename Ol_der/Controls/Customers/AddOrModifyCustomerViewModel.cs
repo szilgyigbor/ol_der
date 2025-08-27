37967d83774f3f3440c6ddc84e05e6f469aa0e14
Gábor <szilgyigbor@gmail.com>
2025-08-27 09:59:03 +0200

    Add customer management user control and view model
    Introduces `AddOrModifyCustomerControl` for adding or modifying customer information, featuring fields for name, address, phone, email, tax number, and notes, along with a submit button. The control uses a resource dictionary for button styles.
    
    Sets up `AddOrModifyCustomerViewModel` as the data context in the code-behind to enable data binding. A new view model class is created, currently without properties or methods.

---
+++ Ol_der/Controls/Customers/AddOrModifyCustomerViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ol_der.Controls.Customers
+{
+    class AddOrModifyCustomerViewModel
+    {
+    }
+}
