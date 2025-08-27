85cb016b4aea0ef72420f6b5429bf3826892cd01
Gábor <szilgyigbor@gmail.com>
2025-08-27 07:53:18 +0200

    Add Customer class to Ol_der.Models namespace
    Introduces a new `Customer` class with properties for
    customer details such as `CustomerId`, `Name`, `Address`,
    `Phone`, `Email`, `TaxNumber`, and `Notes`. The class
    also includes a collection of `Sale` objects to represent
    the relationship between customers and sales. The
    `CustomerId` property is marked for database identity
    generation.

---
+++ Ol_der/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ol_der.Models
+{
+    class Customer
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CustomerId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string TaxNumber { get; set; } = string.Empty;
+        public string Notes { get; set; } = string.Empty;
+
+        public ICollection<Sale> Sales { get; set; } = new List<Sale>();
+    }
+}
