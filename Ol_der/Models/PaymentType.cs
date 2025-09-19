70471e2fcba631c0042b2c66b607d9d993223bcf
Gábor <szilgyigbor@gmail.com>
2025-09-19 11:21:29 +0200

    Add Loan enum, and add Display names.

--- Ol_der/Models/PaymentType.cs
+++ Ol_der/Models/PaymentType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,19 @@ namespace Ol_der.Models
 {
     public enum PaymentType
     {
+        [Display(Name = "Készpénz")]
         Cash,
+
+        [Display(Name = "Kártya")]
         Card,
+
+        [Display(Name = "Átutalás")]
         Transfer,
-        RN
+
+        [Display(Name = "RN")]
+        RN,
+
+        [Display(Name = "Bizomány")]
+        Loan
     }
 }
