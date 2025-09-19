b9816d4ae1705be4d6e33e61f127582691dac88b
Gábor <szilgyigbor@gmail.com>
2025-09-19 11:22:16 +0200

    Add EnumExtensions class.

---
+++ Ol_der/Controls/Sales/EnumExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ol_der.Controls.Sales
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            var member = enumValue.GetType()
+                                  .GetMember(enumValue.ToString())
+                                  .FirstOrDefault();
+
+            var attribute = member?.GetCustomAttribute<DisplayAttribute>();
+
+            return attribute?.Name ?? enumValue.ToString();
+        }
+    }
+}
